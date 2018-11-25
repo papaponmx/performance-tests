be7df6790e57236310ec9353cfc237bbfe2e0e24
papaponmx <jaimemadrigalrios@gmail.com>
2018-11-25 07:22:15 -0600

    Updated app

--- index.js
+++ index.js
@@ -1,33 +1,54 @@
-'use strict';
+
+
 require('dotenv').config();
 
-const fs = require( 'fs' );
-const { lighthousePerfAudits } = require('./audits/lighthousePerfAudits')
+const express = require('express');
+const fs = require('fs');
+const { lighthousePerfAudits } = require('./audits/lighthousePerfAudits');
 
+const app = express();
 const { APP_STAGING_URL, APP_PRODUCTION_URL } = process.env;
-
-
 const flags = {
-  output: 'json'
+  output: 'json',
 };
 
+app.use(bodyParser.json());
 
-lighthousePerfAudits( APP_PRODUCTION_URL, flags )
-  .then( results => {
-     fs.writeFile( '/production_results.json', JSON.stringify(results), function ( err ) {
-        if ( err ) {
-          return console.log( err );
-        }
-        console.log( 'The file was saved!' );
-      } );
-});
+app.get('/run', (req, res) => {
+  // 1. Check secret
+  if (req.headers['X-Hub-Signature']) {
+    console.log('it is working great');
 
-lighthousePerfAudits( APP_STAGING_URL, flags )
-  .then( results => {
-     fs.writeFile( '/staging_results.json', JSON.stringify(results), function ( err ) {
-        if ( err ) {
-          return console.log( err );
-        }
-        console.log( 'The file was saved!' );
-      } );
+    /**
+     * Secret is valid,
+     * run tests and write json data
+     * on both environments
+     */
+    lighthousePerfAudits(APP_PRODUCTION_URL, flags)
+      .then((results) => {
+        fs.writeFile('/production_results.json', JSON.stringify(results), (err) => {
+          if (err) {
+            return console.log(err);
+          }
+          console.log('The file was saved!');
+        });
+      });
+
+    lighthousePerfAudits(APP_STAGING_URL, flags)
+      .then((results) => {
+        fs.writeFile('/staging_results.json', JSON.stringify(results), (err) => {
+          if (err) {
+            return console.log(err);
+          }
+          console.log('The file was saved!');
+        });
+      });
+
+    //       send response with 200
+    res.status(200).send();
+  } else {
+    res.status(500).send();
+  }
+
+  //    secret is not valid, return 500
 });
